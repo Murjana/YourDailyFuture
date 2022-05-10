892236a9b78ca341897d27db9984e52870920b7d
Morjana <96773815+Murjana@users.noreply.github.com>
2022-05-10 15:29:48 +0200

    created flex box with cceptebale size for weather and calendar

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ function App() {
         <h1>Welcome to your daily future!</h1>
       </header>
       <div className="App-body">
-        <div className= 'flex-container'>
-          <div className='calendar'></div>
-          <div className='weather'></div>
-        </div>
+        {/* <div classname= 'main-container'> */}
+          <div className= 'flex-container'>
+            <div className='calendar'></div>
+            <div className='weather'></div>
+          </div>
+        {/* </div> */}
         <p>
           Get to know how your future looks for the day!
         </p>

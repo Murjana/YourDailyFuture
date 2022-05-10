0481e45fca5d3af7ee072274a89b86cd7dd5893e
Morjana <96773815+Murjana@users.noreply.github.com>
2022-05-10 13:34:02 +0200

    adjusted the size of weather and calendar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to your daily future!</h1>
+      </header>
+      <div className="App-body">
         <div className= 'flex-container'>
           <div className='calendar'></div>
           <div className='weather'></div>
         </div>
-
-      </header>
-      <div className="App-body">
         <p>
           Get to know how your future looks for the day!
         </p>

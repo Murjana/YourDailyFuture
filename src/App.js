51f3a66a3a8024121d6deea7fab2a7d5a1f25438
Morjana <96773815+Murjana@users.noreply.github.com>
2022-05-10 16:27:46 +0200

    resized the placeholder of the weather image

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
               <ul className= 'calendar-list'>
                 <li> Coffee with Lisa at 11</li>
                 <li> Stand up meeting at 12</li>
-                <li>Tennis practice at 18</li>
+                <li> Tennis practice at 18</li>
               </ul>
             </div>
             <div className='weather'>
             <h2> Weather</h2>
-            <img src ={sun}/>
+            <img className ="weather-image" src ={sun}/>
             <h3 className='temperature'> 23 degrees </h3>
             </div>
           </div>

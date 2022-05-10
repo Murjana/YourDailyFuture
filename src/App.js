4a6332ae831dabf812bd1bac86416b59eab3b8b8
Morjana <96773815+Murjana@users.noreply.github.com>
2022-05-10 13:18:48 +0200

    deleted the old css code

--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to your daily future!</h1>
         <div className= 'flex-container'>
           <div className='calendar'></div>
           <div className='weather'></div>
         </div>
-          <p>
-          Get to know how your day looks for the day!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <div className="App-body">
+        <p>
+          Get to know how your future looks for the day!
+        </p>
+
+      </div>
     </div>
   );
 }

a3a069fe62c3cf0a3ac6e712bc120210fd5dd14f
Morjana <96773815+Murjana@users.noreply.github.com>
2022-06-01 17:57:09 +0200

    parsed a weather website to reflect todays's weather in the weather section of the project

--- src/App.js
+++ src/App.js
@@ -1,67 +1,80 @@
 import sun from './sun.svg';
 import './App.css';
 import rainy from './rainy.svg';
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 
 
 class CalendarEntry {
-  constructor (description, date) {
-  this.description = description;
-  this.date = date;
-}
+  constructor(description, date) {
+    this.description = description;
+    this.date = date;
+  }
 }
 
 class App extends Component {
 
-  render (){
-  var temperature = Math.round(Math.random()* 30);
-  var image;
-  // todo: use this array directly in html
-  var events = [
-   new CalendarEntry('Coffee with Lisa', '11'),
-   new CalendarEntry('Stand up meeting', '12'),
-   new CalendarEntry('Tennis practice', '18'),
-]
+  render() {
+    if (this.state != null) {
+      var temperature = this.state.todaysTemperature
 
-  if (temperature > 20) {
-    image = sun;
-  }
-  else {
-    image = rainy;
+    }
+    var image;
+    var events = [
+      new CalendarEntry('Coffee with Lisa', '11'),
+      new CalendarEntry('Stand up meeting', '12'),
+      new CalendarEntry('Tennis practice', '18'),
+    ]
 
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to your daily future!</h1>
-      </header>
-      <div className="App-body">
-          <div className= 'flex-container'>
+    if (temperature > 20) {
+      image = sun;
+    }
+    else {
+      image = rainy;
+
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to your daily future!</h1>
+        </header>
+        <div className="App-body">
+          <div className='flex-container'>
             <div className='calendar'>
               <h2> Calendar </h2>
-              <ul className= 'calendar-list'>
+              <ul className='calendar-list'>
                 {
-                 events.map( entry =>
-                     (
-                     <li> {entry.description} at {entry.date} </li>
+                  events.map(entry =>
+                  (
+                    <li> {entry.description} at {entry.date} </li>
                   )
-                 )
+                  )
                 }``
               </ul>
             </div>
             <div className='weather'>
-            <h2> Weather</h2>
-            <img className ="weather-image" src ={image}/>
-            <h3 className='temperature'> {temperature} degrees </h3>
+              <h2> Weather</h2>
+              <img className="weather-image" src={image} />
+              <h3 className='temperature'> {temperature} degrees </h3>
             </div>
           </div>
-        <p>
-          Get to know how your future looks for the day!
-        </p>
+          <p>
+            Get to know how your future looks for the day!
+          </p>
 
+        </div>
       </div>
-    </div>
-);
-}}
+    );
+  }
+  componentDidMount () {
+    fetch ( "https://api.open-meteo.com/v1/forecast?latitude=48.8567&longitude=2.3510&daily=temperature_2m_max&timezone=Europe%2FBerlin" )
+    .then(res => res.json())
+    .then((data) => {
+      this.setState ({ todaysTemperature: data.daily.temperature_2m_max[0]})
+    })
+    .catch(console.log)
+
+  }
+}
+
 
 export default App;
